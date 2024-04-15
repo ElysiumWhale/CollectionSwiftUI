38b0202bd29dd2531684247a8cfe7a5daf0cdea7
Aleksei Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2024-04-15 15:13:29 +0400

    Make properties for tab bar and router

--- CollectionSwiftUISample/CollectionSwiftUISample/AppCoordinator.swift
+++ CollectionSwiftUISample/CollectionSwiftUISample/AppCoordinator.swift
@@ -6,19 +6,22 @@ final class AppCoordinator {
 
     private var cancellables = Set<AnyCancellable>()
 
+    private let tabBar = UITabBarController()
+    private let router = UINavigationController()
+
     init(window: UIWindow) {
         self.window = window
     }
 
     func start() {
         let main = makeMainModule()
-        let navigation = UINavigationController(rootViewController: main)
-        navigation.tabBarItem.title = "Main"
-        navigation.tabBarItem.image = UIImage(systemName: "newspaper")
-        navigation.tabBarItem.selectedImage = UIImage(systemName: "newspaper.fill")
 
-        let tabBar = UITabBarController()
-        tabBar.setViewControllers([navigation], animated: false)
+        router.tabBarItem.title = "Main"
+        router.tabBarItem.image = UIImage(systemName: "newspaper")
+        router.tabBarItem.selectedImage = UIImage(systemName: "newspaper.fill")
+
+        router.setViewControllers([main], animated: true)
+        tabBar.setViewControllers([router], animated: false)
 
         window.rootViewController = tabBar
         window.makeKeyAndVisible()
