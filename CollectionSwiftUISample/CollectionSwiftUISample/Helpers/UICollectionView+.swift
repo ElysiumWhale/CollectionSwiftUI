53a2f47da8ad484d6807c6c63d36616628638cba
Aleksei Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2024-04-16 16:12:16 +0400

    Add collection helpers

---
+++ CollectionSwiftUISample/CollectionSwiftUISample/Helpers/UICollectionView+.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+extension UICollectionView {
+    func dequeue(
+        id: any RawRepresentable<String>,
+        for index: IndexPath
+    ) -> UICollectionViewCell {
+        dequeueReusableCell(withReuseIdentifier: id.rawValue, for: index)
+    }
+}
+
+extension NSCollectionLayoutSection {
+    static func standart(
+        height: NSCollectionLayoutDimension = .estimated(120),
+        width: NSCollectionLayoutDimension = .fractionalWidth(1),
+        isHorizontalGroup: Bool = true
+    ) -> NSCollectionLayoutSection {
+        let size = NSCollectionLayoutSize(
+            widthDimension: width,
+            heightDimension: height
+        )
+
+        let item = NSCollectionLayoutItem(layoutSize: size)
+        let group = isHorizontalGroup
+        ? NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
+        : NSCollectionLayoutGroup.vertical(layoutSize: size, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+}
